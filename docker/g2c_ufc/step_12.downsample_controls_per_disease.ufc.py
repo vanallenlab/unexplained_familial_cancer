5e51930dfaf9ff6274372ae8f34a6c1102e06b2d
Noah Fields <noah_fields@dfci.harvard.edu>
2025-10-26 21:18:19 -0400

    small change

--- docker/g2c_ufc/step_12.downsample_controls_per_disease.ufc.py
+++ docker/g2c_ufc/step_12.downsample_controls_per_disease.ufc.py
@@ -282,14 +282,12 @@ def parse_complex_logic(logic_str, meta):
         elif axis == "2patient":
             optional_cancers = cancer.split('-')
             # Count how many of the optional_cancers are in meta['original_dx']
-            dx_list = [x.strip().lower() for x in meta["original_dx"].split(';')]
-            count = sum(c.lower() in dx_list for c in optional_cancers)
+            count = df['original_dx'].str.lower().apply(lambda x: sum(c in x for c in optional_cancers))
             return count >= 2
         elif axis == "3patient":
             optional_cancers = cancer.split('-')
             # Count how many of the optional_cancers are in meta['original_dx']
-            dx_list = [x.strip().lower() for x in meta["original_dx"].split(';')]
-            count = sum(c.lower() in dx_list for c in optional_cancers)
+            count = df['original_dx'].str.lower().apply(lambda x: sum(c in x for c in optional_cancers))
             return count >= 3
         elif axis == "patient":
             return meta['original_dx'].str.contains(cancer, na=False)
