3ae1f25d4d8ad5aba0a8ea1e49fb85966f121bbe
Noah Fields <noah_fields@dfci.harvard.edu>
2025-12-12 16:31:20 -0500

    fix some bugs

--- scripts/figure_creation/Figure4/4e_history/run_all.sh
+++ scripts/figure_creation/Figure4/4e_history/run_all.sh
@@ -1,4 +1,4 @@
-for f in /Users/noah/Desktop/ufc_repository/results/analysis_4e_results/*.tsv; do
+for f in /Users/noah/Desktop/ufc_repository/results/analysis_4e_results/4e_prs_results/*.tsv; do
     echo "Running on $f"
-    python3 create_figures.4e.py "$f"
+    python3 create_figures.4e.v2.py "$f"
 done
