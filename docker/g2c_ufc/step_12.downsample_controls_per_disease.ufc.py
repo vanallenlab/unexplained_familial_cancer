068bf59f5cb601753396e762348704479c26eacd
Noah Fields <noah_fields@dfci.harvard.edu>
2025-12-30 09:00:37 -0500

    changes to cohort creation

--- docker/g2c_ufc/step_12.downsample_controls_per_disease.ufc.py
+++ docker/g2c_ufc/step_12.downsample_controls_per_disease.ufc.py
@@ -472,6 +472,7 @@ def main():
     # Load sample metadata
     meta = pd.read_csv(args.metadata, sep='\t',index_col=False)
     meta['original_id'] = meta['original_id'].astype(str)
+    meta['case_control'] = (meta['original_dx'] != "control").astype(int)
     phenotype_data = pd.read_csv(args.phenotype_data,sep='\t',index_col=False)
     phenotype_data['Sample'] = phenotype_data['Sample'].astype(str)
     phenotype_data = phenotype_data[~((phenotype_data['cancer'] == "control") & (phenotype_data['maternal_family_dx'].notna() | phenotype_data['paternal_family_dx'].notna()))]
